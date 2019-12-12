3733188ffbce7059d7ec00f4ad0d0b4ee571baf6
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2019-12-12 08:44:03 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/skovde/App.java
@@ -0,0 +1,27 @@
+package se.lexicon.skovde;
+
+/**********************************************************
+ Water
+
+ Boiling point of water	    212°F	    100°C
+ Freezing point of water	32°F	    0°C
+ Absolute zero	            -459.6°F	-273.1°C
+
+ **********************************************************
+ Person
+
+ Hypothermia	        <35.0°C	            95.0°F
+ Normal	                36.5 - 37.5°C	    97.7 - 99.5°F
+ Fever / Hyperthermia	>37.5 or 38.3°C	    99.5 or 100.9°F
+ Hyperpyrexia	        >40.0 or 41.5°C	    104.0 or 106.7°F
+
+ ***********************************************************/
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+
+        System.out.println(  );
+    }
+}
